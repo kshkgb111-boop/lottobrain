3e1d4773f8d975d2be3771727b005cbaf46efb38
강기봉 <gang-gibong@gang-gibong-ui-MacBookAir.local>
2026-04-15 21:54:15 +0900

    fix: pin python 3.12, suppress SSL warnings

--- lotto_core.py
+++ lotto_core.py
@@ -12,6 +12,8 @@ from datetime import datetime
 
 try:
     import requests
+    import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 except ImportError:
     requests = None
 
